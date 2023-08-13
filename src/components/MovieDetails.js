f024a6e98695793886a59a1e7a01ac973bff7557
Ahmed Said <ahmedsaidadnan@outlook.com>
2023-08-13 03:46:22 +0300

    fixed API

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -82,7 +82,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     function () {
       async function getMovieDetails() {
         setIsLoading(true);
-        const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`);
+        const res = await fetch(`https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`);
         const data = await res.json();
         setMovie(data);
         setIsLoading(false);
